2fe1bdcdfac23de4ea2bd3e61e993ff9180ff710
Arda Furkan Aslanbas <aslanbasardafurkan@gmail.com>
2026-07-17 14:08:37 +0300

    fix: proxy /_next assets to nextjs dev server to resolve dev white screen

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
+      '/_next': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
     },
   },
 })
